743bc5858c54312cedaaa742d4fa14d17bda66f3
sudonetizen <sardortolibjonov1039@gmail.com>
2026-04-06 21:07:44 +0500

    add UserLoginView to account app

---
+++ account/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+from .forms import LoginForm
+
+
+class UserLoginView(View):
+    def get(self, request):
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
+
+    def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            
+            if user is not None: 
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+
+        
