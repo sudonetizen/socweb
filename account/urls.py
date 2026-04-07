18ea552dbdffed41f7ef1143a3b92a49498d3518
sudonetizen <sardortolibjonov1039@gmail.com>
2026-04-08 03:54:14 +0500

    add UserEditView

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import dashboard, UserRegisterView
+from .views import dashboard, UserRegisterView, UserEditView
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('', dashboard, name='dashboard'),
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('edit/', UserEditView.as_view(), name='edit'),
 ]
 
--- account/views.py
+++ account/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-from .forms import LoginForm, UserRegistrationForm 
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 
 
 @login_required
@@ -22,6 +23,9 @@ class UserRegisterView(View):
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
+
+            Profile.objects.create(user=new_user)
+
             return render(request, 'register_done.html', {'new_user': new_user})
 
 class UserLoginView(View):
@@ -45,3 +49,18 @@ class UserLoginView(View):
                 return HttpResponse('Invalid login')
 
         
+class UserEditView(LoginRequiredMixin, View):
+    def get(self, request):
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+        return render(request, 'edit.html', {'user_form': user_form, 'profile_form': profile_form})
+
+    def post(self, request):
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+        
+        return render(request, 'edit.html', {'user_form': user_form, 'profile_form': profile_form})
+
