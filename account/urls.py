614c408dcff241fffc4c110e8bb24268939ea181
sudonetizen <sardortolibjonov1039@gmail.com>
2026-04-08 00:49:49 +0500

    add password reset

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,9 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
 from .views import dashboard
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_chabnge'),
-    path('password-change/done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('', include('django.contrib.auth.urls')),
     path('', dashboard, name='dashboard'),
 ]
 
--- django_project/settings.py
+++ django_project/settings.py
@@ -126,3 +126,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = 'dashboard'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
