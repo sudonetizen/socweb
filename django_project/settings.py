b091dd1cf19b893eefb19546b75be38d1a71eaaa
sudonetizen <sardortolibjonov1039@gmail.com>
2026-04-08 02:52:21 +0500

    add Profile model

--- django_project/settings.py
+++ django_project/settings.py
@@ -128,3 +128,6 @@ LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = 'dashboard'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
