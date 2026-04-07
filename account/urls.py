e4b2748efeb2d9d1419a80fabb8c3e74b7cd4e43
sudonetizen <sardortolibjonov1039@gmail.com>
2026-04-08 00:09:15 +0500

    add password change

--- account/urls.py
+++ account/urls.py
@@ -5,5 +5,8 @@ from .views import dashboard
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_chabnge'),
+    path('password-change/done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('', dashboard, name='dashboard'),
 ]
+
