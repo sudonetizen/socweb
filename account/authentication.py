12be1fd54cc6e20b9ac578dadbe6fce3acb68da0
sudonetizen <sardortolibjonov1039@gmail.com>
2026-04-14 23:40:33 +0500

    add create profile function

--- account/authentication.py
+++ account/authentication.py
@@ -1,3 +1,4 @@
+from account.models import Profile 
 from django.contrib.auth.models import User
 
 class EmailAuthBackend:
@@ -15,3 +16,8 @@ class EmailAuthBackend:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+
+
+def create_profile(backend, user, *args, **kwargs):
+    Profile.objects.get_or_create(user=user)
+
