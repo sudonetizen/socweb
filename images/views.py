05829629489c642b13bab658c18cfce4afb3ce40
sudonetizen <sardortolibjonov1039@gmail.com>
2026-04-16 09:19:02 +0500

    add ImageCreateView to images app

--- images/views.py
+++ images/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View
+from .forms import ImageCreateForm
 
-# Create your views here.
+
+class ImageCreateView(LoginRequiredMixin, View):
+    def get(self, request):
+        form = ImageCreateForm(data=request.GET)
+        return render(request, 'img_create.html', {'section':'images', 'form':form}) 
+
+    def post(self, request):
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_image = form.save(commit=False)
+            new_image.user = request.user
+            new_image.save()
+
+            messages.success(request, 'Image added successfully')
+            
+            return redirect(new_image.get_absolute_url())
+
+        return render(request, 'img_create.html', {'section':'images', 'form':form}) 
+
