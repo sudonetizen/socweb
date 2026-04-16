c6b1b7e26b6d9cdaba278a6f690f959505069777
sudonetizen <sardortolibjonov1039@gmail.com>
2026-04-16 08:40:58 +0500

    add save method to ImageCreateForm

--- images/forms.py
+++ images/forms.py
@@ -1,4 +1,7 @@
+import requests
 from django import forms
+from django.utils.text import slugify
+from django.core.files.base import ContentFile
 from .models import Image
 
 class ImageCreateForm(forms.ModelForm):
@@ -20,4 +23,24 @@ class ImageCreateForm(forms.ModelForm):
         
         return url
 
+    
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slufigy(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()        
 
+        image_name = f'{name}.{extentsion}'
+        response = requests.get(image_url)
+        
+        image.image.save(
+            image_name,
+            ContentFile(response.content),
+            save=False
+        )
+
+        if commit:
+            image.save()
+
+        return image
+        
