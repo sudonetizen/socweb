04a48225bdcd0b5c84df4a4b9c7867d26dc4c578
sudonetizen <sardortolibjonov1039@gmail.com>
2026-04-16 07:36:27 +0500

    update images model of images app

--- images/models.py
+++ images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 
 
 class Image(models.Model):
@@ -12,6 +13,8 @@ class Image(models.Model):
     description = models.TextField(blank=True)
     created= models.DateTimeField(auto_now_add=True)
 
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
+
     class Meta:
         indexes = [
             models.Index(fields=['-created']),
@@ -21,4 +24,9 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
