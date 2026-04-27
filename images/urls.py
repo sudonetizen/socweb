9623d10613787a4a5d64e136777401e30c276e26
sudonetizen <sardortolibjonov1039@gmail.com>
2026-04-27 23:08:32 +0500

    update: urls of image app

--- images/urls.py
+++ images/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ImageCreateView, ImageLikeView, image_detail
+from .views import ImageCreateView, ImageLikeView, ImageListView, image_detail
 
 app_name = 'images'
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path('create/', ImageCreateView.as_view(), name='create'),
     path('detail/<int:id>/<slug:slug>/', image_detail, name='detail'),
     path('like/', ImageLikeView.as_view(), name='like'),
+    path('', ImageListView.as_view(), name='list'),
 ]
 
 
