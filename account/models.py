927507c533f6a6b7210f147995f3cb4b85bfc89d
sudonetizen <sardortolibjonov1039@gmail.com>
2026-05-02 23:51:05 +0500

    update: user model

--- account/models.py
+++ account/models.py
@@ -3,21 +3,6 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    data_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
-
-    user_model = get_user_model()
-    user_model.add_to_class(
-        'following',
-        models.ManyToManyField('self', through=Contact, related_name='followers', symmetrical=False)
-    )
-
-    def __str__(self):
-        return f'Profile of {self.user.username}'
-
-
 class Contact(models.Model):
     user_from = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -41,3 +26,20 @@ class Contact(models.Model):
 
     def __str__(self):
         return f'{self.user_from} follows {self.user_to}'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    data_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+
+    user_model = get_user_model()
+    user_model.add_to_class(
+        'following',
+        models.ManyToManyField('self', through=Contact, related_name='followers', symmetrical=False)
+    )
+
+    def __str__(self):
+        return f'Profile of {self.user.username}'
+
+
