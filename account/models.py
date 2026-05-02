4b33b795e674d2b335259052c8651589276b558a
sudonetizen <sardortolibjonov1039@gmail.com>
2026-05-02 23:48:48 +0500

    update: user model

--- account/models.py
+++ account/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     data_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+
+    user_model = get_user_model()
+    user_model.add_to_class(
+        'following',
+        models.ManyToManyField('self', through=Contact, related_name='followers', symmetrical=False)
+    )
 
     def __str__(self):
         return f'Profile of {self.user.username}'
