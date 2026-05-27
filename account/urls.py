234d89b9c9cb2d9c0a86abcffe02f7ec7686a09b
sudonetizen <sardortolibjonov1039@gmail.com>
2026-05-27 17:34:14 +0500

    upd: urls of account app

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import dashboard, UserRegisterView, UserEditView, user_list, user_detail
+from .views import dashboard, UserRegisterView, UserEditView, user_list, user_detail, user_follow
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
@@ -7,7 +7,7 @@ urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('edit/', UserEditView.as_view(), name='edit'),
     path('users/', user_list, name='user_list'),
-    path('users/', user_list, name='user_list'),
+    path('users/follow/', user_follow, name='user_follow'),
     path('users/<username>/', user_detail, name='user_detail'),
 ]
 
